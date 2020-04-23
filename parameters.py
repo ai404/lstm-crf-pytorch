d204d14a8fef552f697cadbd95b428e96524f2e5
threelittlemonkeys <sjlee7642@gmail.com>
2020-04-23 11:37:33 +0900

    Update by commit

--- parameters.py
+++ parameters.py
@@ -4,7 +4,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 UNIT = "word" # unit of tokenization (char, word, sent)
-TASK = "sentence-segmentation" # task (None, word-segmentation, sentence-segmentation)
+TASK = None # task (None, word-segmentation, sentence-segmentation)
 RNN_TYPE = "LSTM" # LSTM or GRU
 NUM_DIRS = 2 # unidirectional: 1, bidirectional: 2
 NUM_LAYERS = 2
